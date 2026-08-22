0619d5f46d05ddf46737c2574c7a229485b2ecfb
Artur Zarzecki <artur.zarzecki@techsterowniki.pl>
2026-08-22 11:36:21 +0200

    test(esp32p4): add JC8012 10-inch rotation smoke apps

---
+++ apps/esp32p4/jc8012_10/display_touch_smoke/extra_script.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+Import("env")
+project_dir = Path(env.subst("$PROJECT_DIR"))
+brick_root = project_dir.parents[3] / "libs" / "brick"
+env.Append(CPPPATH=[str(brick_root / "libs" / "interfaces" / "include"), str(brick_root / "libs" / "core" / "include"), str(brick_root / "platforms" / "esp32" / "include")])
+brick_build_dir = Path(env.subst("$BUILD_DIR")) / "brick"
+env.BuildSources(str(brick_build_dir / "core"), str(brick_root / "libs" / "core" / "src"), "+<TouchMapper.cpp>")
+env.BuildSources(str(brick_build_dir / "touch"), str(brick_root / "platforms" / "esp32" / "src"), "+<Gsl3680Touchscreen.cpp>")
+env.BuildSources(str(brick_build_dir / "platform"), str(brick_root / "platforms" / "esp32" / "src"), "+<MipiDsiDisplay.cpp>")
