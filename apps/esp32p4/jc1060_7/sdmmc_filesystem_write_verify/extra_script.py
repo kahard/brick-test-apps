3aba6a9177719277798c26b141de0c63e3d4ce52
Artur Zarzecki <artur.zarzecki@techsterowniki.pl>
2026-08-27 18:59:38 +0200

    Add ESP32-P4 SDMMC write verify app

---
+++ apps/esp32p4/jc1060_7/sdmmc_filesystem_write_verify/extra_script.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+Import("env")
+
+project_dir = Path(env.subst("$PROJECT_DIR"))
+brick_root = project_dir.parents[3] / "libs" / "brick"
+env.Append(CPPPATH=[
+    str(brick_root / "libs" / "interfaces" / "include"),
+    str(brick_root / "libs" / "core" / "include"),
+    str(brick_root / "platforms" / "esp32" / "include"),
+])
+brick_build_dir = Path(env.subst("$BUILD_DIR")) / "brick"
+env.BuildSources(str(brick_build_dir / "platform"), str(brick_root / "platforms" / "esp32" / "src"), "+<SdmmcFileSystem.cpp>")
+
