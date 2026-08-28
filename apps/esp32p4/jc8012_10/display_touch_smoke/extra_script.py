cc6fd68d3565f0860e2a052797181fb7545084df
Artur Zarzecki <artur.zarzecki@techsterowniki.pl>
2026-08-28 06:26:34 +0200

    Add public board include path to ESP32-P4 builds

--- apps/esp32p4/jc8012_10/display_touch_smoke/extra_script.py
+++ apps/esp32p4/jc8012_10/display_touch_smoke/extra_script.py
@@ -3,7 +3,7 @@ from pathlib import Path
 Import("env")
 project_dir = Path(env.subst("$PROJECT_DIR"))
 brick_root = project_dir.parents[3] / "libs" / "brick"
-env.Append(CPPPATH=[str(brick_root / "libs" / "interfaces" / "include"), str(brick_root / "libs" / "core" / "include"), str(brick_root / "platforms" / "esp32" / "include")])
+env.Append(CPPPATH=[str(brick_root / "libs" / "interfaces" / "include"), str(brick_root / "libs" / "core" / "include"), str(brick_root / "platforms" / "esp32" / "include"), str(project_dir.parents[3] / "libs" / "brick-boards" / "include")])
 brick_build_dir = Path(env.subst("$BUILD_DIR")) / "brick"
 env.BuildSources(str(brick_build_dir / "core"), str(brick_root / "libs" / "core" / "src"), "+<TouchMapper.cpp>")
 env.BuildSources(str(brick_build_dir / "touch"), str(brick_root / "platforms" / "esp32" / "src"), "+<Gsl3680Touchscreen.cpp>")
